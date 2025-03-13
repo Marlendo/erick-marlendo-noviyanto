5b63c0683f23ac3d09963ee86181049d58de06bc
marlendo <erikmarlendo@gmail.com>
2025-03-13 22:26:55 +0700

    [SERVICES] | completed feature services

--- src/screens/cv/myService/index.tsx
+++ src/screens/cv/myService/index.tsx
@@ -1,8 +1,8 @@
 import { Title } from "#/components/headers";
-import { skillService } from "#/services/skill";
+import { serviceService } from "#/services/services";
 
 function MyService() {
-  const skill = skillService();
+  const services = serviceService();
   return (
     <div className=" min-h-[100vh]">
       <Title title="Services" subtitle="02" />
@@ -17,6 +17,21 @@ function MyService() {
         <h2 className="inline-block font-titillium text-xl">
           Our services include:
         </h2>
+      </div>
+      <div className="my-[20px]">
+        {
+          services.map((item, index) => (
+            <div key={`service-${index}`} className="my-[50px]">
+              <div className="flex flex-row justify-start items-center">
+                <div dangerouslySetInnerHTML={{ __html: item.icon }} className="w-[100px] mr-[20px]" />
+                <div>
+                  <h3 className="text-2xl font-bold">{item.title}</h3>
+                  <p className="text-sm">{item.subtitle}</p>
+                </div>
+              </div>
+            </div>
+          ))
+        }
       </div>
     </div>
   );
